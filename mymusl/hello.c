64c5a8529fff43a77c97369466864681a9046439
celestee <gaokailong65@foxmail.com>
2026-08-26 16:07:22 +0800

    fix: time interrupt

--- mymusl/hello.c
+++ mymusl/hello.c
@@ -3,14 +3,14 @@
 #include <limits.h>
 int main()
 {
-	int a = 10;
+	int a = 400;
 	printf("hello:Hello World!\n");
 	printf("hello:Before loop.\n");
 	while (a>0)
 	{
 		a--;
 		printf("hello:a=%d\n",a);
-		sched_yield();
+		// sched_yield();
 	}
 	
 	printf("hello:Finish loop.\n");
